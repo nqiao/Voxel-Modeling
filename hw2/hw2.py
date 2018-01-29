ca2f0925f80ce21ca8ec910d441f4807890c1a95
nqiao <qiaonan1992@gmail.com>
2018-01-29 09:49:35 -0800

    revise sin

--- hw2/hw2.py
+++ hw2/hw2.py
@@ -24,7 +24,7 @@ def fArray(x, y):
 
 
 @cuda.jit(device=True)
-def f2D(x, y):
+def f2D_cuda(x, y):
     return math.sin(np.pi * x) * math.sinh(np.pi * y) / math.sinh(np.pi)
 
 
@@ -33,7 +33,7 @@ def fKernel2D(d_x, d_y, d_f):
     i, j = cuda.grid(2)
     nx, ny = d_f.shape
     if i < nx and j < ny:
-        d_f[i, j] = f2D(d_x[i], d_y[i])
+        d_f[i, j] = f2D_cuda(d_x[i], d_y[i])
 
 
 def fArray2D(x, y):
@@ -48,49 +48,56 @@ def fArray2D(x, y):
     return d_f.copy_to_host()
 
 
+# Problem 5
 @cuda.reduce
 def sum_reduce(x, y):
     return x + y
 
 
 @cuda.jit
-def normKernel(d_x, d_y, d_f, p):
+def normKernel(d_f, p):
     i, j = cuda.grid(2)
     nx, ny = d_f.shape
     if i < nx and j < ny:
-        d_f[i, j] = math.sin(d_x[i]) ** p + math.sin(d_y[j]) ** p
+        d_f[i][j] = d_f[i][j] ** p
 
 
 def lp_norm(p):
     N = 256
-    scale = np.linspace(0, 2 * np.pi, 256)
-    x = np.sin(scale)
-    y = np.sin(scale)
-    d_x = cuda.to_device(x)
-    d_y = cuda.to_device(y)
-    d_f = cuda.device_array((N, N), dtype = np.float32)
+    x = np.linspace(0, 1, 256)
+    y = np.linspace(0, 1, 256)
+    f = np.empty(256 * 256).reshape((256, 256))
+    for i in range(256):
+        for j in range(256):
+            f[i][j] = math.sin(2 * np.pi * x[i]) * math.sin(2 * np.pi * y[j])
+    d_f = cuda.to_device(f)
     gridDims = ((N + TPBX - 1) // TPBX, (N + TPBY - 1) // TPBY)
     blockDims = (TPBX, TPBY)
-    normKernel[gridDims, blockDims](d_x, d_y, d_f, p)
+    normKernel[gridDims, blockDims](d_f, p)
     f = d_f.copy_to_host()
+    print(f.shape)
     f_1 = f.flatten()
     return math.pow(sum_reduce(f_1), 1/p)
 
 
 def serial_norm(p):
     N = 256
-    scale = np.linspace(0, 2 * np.pi, 256)
-    x = np.sin(scale)
-    y = np.sin(scale)
+    x = np.linspace(0, 1, 256)
+    y = np.linspace(0, 1, 256)
+    f = np.empty(256 * 256).reshape((256, 256))
+    for i in range(256):
+        for j in range(256):
+            f[i][j] = math.sin(2 * np.pi * x[i]) * math.sin(2 * np.pi * y[j])
     sum = 0
     for i in range(256):
         for j in range(256):
-            sum += x[i] ** p + y[j] ** p
+            sum += f[i][j] ** p
     return math.pow(sum, 1/p)
 
 def main():
-    # x = np.random.rand(100)
-    # y = np.random.rand(100)
+    # # Problem 2
+    # x = np.random.rand(10000)
+    # y = np.random.rand(10000)
     
     # start_s = time()
     # f_s = fArray(x, y)
@@ -106,9 +113,11 @@ def main():
     # print("parallel", time_p)
     # # print(f_p)
     
+
+    # Problem 5
     p = 2
     print("norm, p = ", p)
-    # print(lp_norm(p))
+    print(lp_norm(p))
     print("serial norm, p = ", p)
     print(serial_norm(p))
 
